6668589e265de19c30ef07123f58eebe65673986
Vikash <vikashmishra2018@gmail.com>
2025-08-26 15:18:55 +0530

    this is commit from Asian Time Zone guy

---
+++ src/test/java/rahulshettyacademy/SeleniumFrameworkDesign/GitDemoForGitX.java
@@ -0,0 +1,11 @@
+package rahulshettyacademy.SeleniumFrameworkDesign;
+
+import org.testng.annotations.Test;
+
+public class GitDemoForGitX {
+
+	@Test
+	public void GitX() {
+		System.out.println("GitX continue from here");
+		}
+}
