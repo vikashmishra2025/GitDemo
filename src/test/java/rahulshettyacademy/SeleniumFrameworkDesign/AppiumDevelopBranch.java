1dabe547aa681e06c26899a0475f364c600aacb5
Vikash <vikashmishra2018@gmail.com>
2025-08-28 13:38:15 +0530

    Architect Asian development

---
+++ src/test/java/rahulshettyacademy/SeleniumFrameworkDesign/AppiumDevelopBranch.java
@@ -0,0 +1,12 @@
+package rahulshettyacademy.SeleniumFrameworkDesign;
+
+public class AppiumDevelopBranch {
+
+	public static void main(String[] args) {
+		System.out.println("IOSmaps");
+		System.out.println("IOSapps");
+		System.out.println("IOSmsg");
+
+	}
+
+}
